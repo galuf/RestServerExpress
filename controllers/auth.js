4d63dac671a75cb916d6151b932e2c07cdc50679
galuf <alex.flores.m@uni.pe>
2021-09-13 09:47:06 -0500

    google login

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,7 +2,9 @@ const { response } = require("express");
 const bcryptjs = require("bcryptjs");
 
 const Usuario = require("../models/usuario");
+
 const { generarJWT } = require("../helpers/generar-jwt");
+const { googleVerify } = require("../helpers/google-verify");
 
 const login = async (req, res = response) => {
   const { correo, password } = req.body;
@@ -43,6 +45,48 @@ const login = async (req, res = response) => {
   }
 };
 
+const googleSignin = async (req, res = response) => {
+  const { id_token } = req.body;
+
+  try {
+    const { correo, nombre, img } = await googleVerify(id_token);
+
+    let usuario = await Usuario.findOne({ correo });
+    if (!usuario) {
+      //Tengo q crearlo
+      const data = {
+        nombre,
+        correo,
+        password: ":þ",
+        img,
+        google: true,
+      };
+
+      usuario = new Usuario(data);
+      await usuario.save();
+    }
+
+    if (!usuario.estado) {
+      return res.status(401).json({
+        msg: "Usuario bloqueado",
+      });
+    }
+
+    //Generar el JWT
+    const token = await generarJWT(usuario.id);
+
+    res.json({
+      usuario,
+      token,
+    });
+  } catch (error) {
+    res.status(400).json({
+      msg: "Token de google no es valido",
+    });
+  }
+};
+
 module.exports = {
   login,
+  googleSignin,
 };
